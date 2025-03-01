49b951d63d9af506bc504e676f19b1b5fe7fd7d5
aredio <aredio.filho@gmail.com>
2025-03-01 10:56:11 -0300

    add allorigins api

--- folio/js/api.js
+++ folio/js/api.js
@@ -1,7 +1,8 @@
-
 async function fetchProfileData() {
-    const url = 'https://aredio.github.io/devs/folio/data/profile.json';
-    const response = await fetch(url)
-    const profileData = await response.json()
-    return profileData
+    const proxyUrl = 'https://api.allorigins.win/get?url=';
+    const targetUrl = encodeURIComponent('https://github.com/aredio/devs/raw/refs/heads/main/folio/data/profile.json');
+
+    const response = await fetch(`${proxyUrl}${targetUrl}`);
+    const data = await response.json();
+    return JSON.parse(data.contents);
 }
