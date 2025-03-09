40cfebd1a60c8e1fe2ee8ce2c6b1889e815e2c5c
aredio <aredio.filho@gmail.com>
2025-03-09 17:21:24 -0300

    merging versions

--- js/api.js
+++ js/api.js
@@ -1,8 +1,6 @@
 async function fetchProfileData() {
-    const proxyUrl = 'https://api.allorigins.win/get?url=';
-    const targetUrl = encodeURIComponent('https://github.com/aredio/devs/raw/refs/heads/main/folio/data/profile.json');
+    const url = encodeURIComponent('https://raw.githubusercontent.com/aredio/devs/refs/heads/gh-pages/data/profile.json');
 
-    const response = await fetch(`${proxyUrl}${targetUrl}`);
-    const data = await response.json();
-    return JSON.parse(data.contents);
+    const fetching = await fetch(url);
+    return await fetching.json();
 }
