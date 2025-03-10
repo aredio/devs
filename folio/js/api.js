8cf067bbddd9e49cea46d492dde77584b1160ab1
aredio <aredio.filho@gmail.com>
2025-03-10 20:11:33 -0300

    code polishment

--- folio/js/api.js
+++
@@ -1,6 +0,0 @@
-async function fetchProfileData() {
-    const url = encodeURIComponent('https://raw.githubusercontent.com/aredio/devs/refs/heads/gh-pages/data/profile.json');
-
-    const fetching = await fetch(url);
-    return await fetching.json();
-}
--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = encodeURIComponent('https://raw.githubusercontent.com/aredio/devs/refs/heads/gh-pages/data/profile.json');
+    const url = 'https://raw.githubusercontent.com/aredio/devs/refs/heads/gh-pages/data/profile.json';
 
     const fetching = await fetch(url);
     return await fetching.json();
